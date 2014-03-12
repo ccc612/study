15a8cfe1a56190e5583d507e26d3f3597ea8ee10
Minchul Jung <ccc612@gmail.com>
2014-03-12 15:56:10 +0900

    re-implements AJAX example with webworker
    re-implements AJAX example with webworker
    - changing page request will be sent to webworker
    - webworker loads target page
    - webworker sends target content to main page
    - main page changes content of main layer

---
+++ html5/WebContent/sessionHistory/javascripts/worker.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+var page = new XMLHttpRequest();
+
+page.onload = function() {
+	postMessage(page.responseText);
+}
+
+function messageHandler(e) {
+	if (e.data.key) {
+		page.open('GET', '../' + e.data.key, true);
+		page.send();
+	}		
+}
+
+addEventListener("message", messageHandler, true);
